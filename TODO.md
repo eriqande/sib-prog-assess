2b3587b73d9859f56e6bb07073e808371476a99a
Eric C. Anderson <eric.anderson@noaa.gov>
2014-05-15 23:18:16 -0700

    Got the CompileSuppRefs script working and updated the suppl_cross_refs.aux.
    Also changed supplement numbers in main text.

--- TODO.md
+++ TODO.md
@@ -1,6 +1,11 @@
 
 
 1. Get the appendix typesetting squared away
-2. Make a compile-document.R script
+2. Make a compile-document.R script.  Perhaps move the files from R-master into that, 
+and keep it at the top level
 3. Updated REAMDE to explain that, and to explain that the main document figures do not currently get regenerated.
+4. Include the CompileSuppCrossRefs step in the compile-document.R 
+5. Write a clean-up script.
+6. Swap the production quality pdf smearograms in for the jpg previews in the main document.
+
 
--- script/CompileSuppRefs.sh
+++ script/CompileSuppRefs.sh
@@ -1,8 +1,6 @@
 
-# this should be run in the directory:
-# /Users/eriq/prj/AlmudevarCollab/SibProgramsEvaluation/doc
-for i in 1 2 3 4; do
-	awk 'BEGIN {OFS="{"; } /^\\newlabel{[^s]/ {a=$0; split(a,b,/{/); print b[1],b[2],b[3],b[4],b[5],b[6] "}" }' suppl$i/suppl$i.aux
-done > sib_assess_tex/suppl_cross_refs.aux
-
-
+# this should be run in the top directory of the repository
+# (the directory that contains suppl and sib_assess_tex, etc:
+for i in ./suppl/appendices/appendices.aux ./suppl/biggest_sibship/biggest_sibship.aux ./suppl/boxplots/boxplots.aux ./suppl/running_times/running_times.aux ./suppl/smearograms/smearograms.aux; do
+	awk 'BEGIN {OFS="{"; } /^\\newlabel{[^s]/ {a=$0; split(a,b,/{/); print b[1],b[2],b[3],b[4],b[5],b[6] "}" }'  $i
+done  > sib_assess_tex/suppl_cross_refs.aux
