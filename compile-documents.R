032a3c645b8b8ff21b2d01e256d15dbd127c5640
Eric C. Anderson <eric.anderson@noaa.gov>
2014-05-16 12:27:55 -0700

    Create a single compile-documents script
    Have pretty much everything running now.  Ready to edit the text and finish the paper!

---
+++ compile-documents.R
@@ -0,0 +1,80 @@
+# This is the main R script that will create the plots and latex
+# The documents.  
+
+# Source it from the directory that contains sib-proj-assess.Rproj
+# It will create the directory Compiled_Documents that has all the typeset
+# PDFs in it.
+
+
+# check that you are in the right directory
+if(any(!file.exists("suppl", "sib-prog-assess.Rproj"))) stop("It appears you are not running compile-documents.R in the correct directory")
+
+
+
+#### Generate the supplement plots.  Output goes to ./tmp/plots/ ####
+source("R/all_smearograms.R")  # make the smearogram plots.  Ouput goes to ./tmp/plots/smearograms
+source("R/BiggestSibshipAndSingletons.R") # output goes to ./tmp/plots/biggest_sibship
+source("R/make_boxplots.R")   # output goes to  ./tmp/plots/boxplots
+source("R/make_running_time_supplement_plots.R")  # output goes to  ./tmp/plots/running_times
+
+
+
+
+#### Generate the figures for the main document
+#### NOTE! THIS IS DISABLED AT THE MOMENT.  NOT PORTABLE. 
+# source("R/make-main-text-figures.R")
+# Note that the above, even if run, does not do the final copying of 
+# figures to the sib_assess_tex/images as would be needed.
+
+
+
+
+#### LaTeX the supplement plots.  Output created in subdirectories
+#### under ./suppl and final PDFs are copied to Compiled_Documents  ####
+# Names of supplements in desired order
+SUPPS <- c("appendices", "smearograms", "boxplots", "biggest_sibship", "running_times") 
+COMP_DOC_DIR <- "Compiled_Documents"
+dir.create(COMP_DOC_DIR)
+
+curdir <- getwd()
+
+M<-1
+for(i in SUPPS) {
+  setwd(file.path(curdir, "suppl", i))
+  latex_call <- paste("latexmk -pdf", i)
+  system(latex_call)
+  final.file.name <- paste("suppl_", M, "_", i, ".pdf", sep="")
+  file.copy(paste(i, "pdf", sep="."), file.path(curdir, COMP_DOC_DIR, final.file.name))
+  M <- M + 1
+}
+
+# at the end, change back to the original directory
+setwd(curdir)
+
+# and then a very important step: transfer the .aux files (and for some reason
+# they need a little processing) over to sib_assess_tex so that the 
+# cross references to the supplement figures come out correctly.
+# the following call updates sib_assess_tex/suppl_cross_refs.aux
+system("./script/CompileSuppRefs.sh")
+
+
+
+
+#### LaTeX the 2-column and the 1-column version of the main document   ####
+setwd(file.path(curdir, "sib_assess_tex"))
+system("latexmk -pdf sib_prog_eval_two_columns")   # make the two column version
+
+# now we generate a main-body-text file with no starred sidewaysfigures for the one column version
+mbt <- readLines("main-body-text.tex")
+mbtns <- gsub("sidewaysfigure\\*", "sidewaysfigure", mbt)  # remove the *'s
+cat(mbtns, sep="\n", file = "main-body-text-one-col.tex")
+
+# then latex the one column version
+system("latexmk -pdf sib_prog_eval")
+
+# now copy those documents to the compiled docs directory
+file.copy(c("sib_prog_eval_two_columns.pdf", "sib_prog_eval.pdf"), file.path(curdir, COMP_DOC_DIR))
+
+# at the end, change back to the original directory
+setwd(curdir)
+
